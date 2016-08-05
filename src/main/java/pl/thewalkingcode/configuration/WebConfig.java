96f0e306720a4e9e520ff5d225b6a0bf0c83cb6e
Marek Chrapek <marek.chrapek@thewalkingcode.pl>
2016-08-05 13:04:08 +0200

    add thymeleaf

--- src/main/java/pl/thewalkingcode/configuration/WebConfig.java
+++ src/main/java/pl/thewalkingcode/configuration/WebConfig.java
@@ -4,9 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 import pl.thewalkingcode.controller.HomeControllerComponentScanner;
+
+import javax.swing.*;
 
 
 @EnableWebMvc
@@ -15,11 +24,33 @@ import pl.thewalkingcode.controller.HomeControllerComponentScanner;
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setSuffix(".jsp");
-        viewResolver.setPrefix("/WEB-INF/");
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
         return viewResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return  templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("img/**").addResourceLocations("/img/");
     }
 
 }
