b092150871eaf8a4f259f29ee1be677216bfcf7a
Marek Chrapek <marek.chrapek@thewalkingcode.pl>
2016-08-05 16:58:20 +0200

    add jquery

--- src/main/java/pl/thewalkingcode/configuration/WebConfig.java
+++ src/main/java/pl/thewalkingcode/configuration/WebConfig.java
@@ -49,6 +49,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("css/**").addResourceLocations("/css/");
         registry.addResourceHandler("img/**").addResourceLocations("/img/");
+        registry.addResourceHandler("/font/**").addResourceLocations("/font/");
     }
 
 }
