7dc174fbdd2701b2c4bd10aca41fdbcc85345b5a
Marek Chrapek <marek.chrapek@thewalkingcode.pl>
2016-08-08 15:30:19 +0200

    add sidebar animation

--- src/main/webapp/script/sidebar.js
+++ src/main/webapp/script/sidebar.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
 
     $("#navico").click(function () {
-        $("#sidebar").animate({
-            width: '16%'
-        });
-        $("#navico").hide();
+        var width = $('#sidebar').css('width');
+        if(width === '165px') {
+            $("#sidebar").animate({
+                width: '65px'
+            });
+            $("#navico").removeClass('fa-arrow-left').addClass('fa-bars');
+            $("#login-menu").hide();
+            $("#main-menu").hide();
+        } else {
+            $("#sidebar").animate({
+                width: '165px'
+            });
+            $("#login-menu").show();
+            $("#main-menu").show();
+            $("#navico").removeClass('fa-bars').addClass('fa-arrow-left');
+        }
     });
 
 });
